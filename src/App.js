21a502935bbb069d0d6c513bd9679c417732dfeb
Peter Sims <ragtimesims@gmail.com>
2019-07-04 22:00:43 -0400

    finish hookifying

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Navbar from './Navbar';
 import Form from './Form';
 import PageContent from './PageContent';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { LanguageProvider } from './contexts/LanguageContext';
 
-class App extends Component {
-	render() {
-		return (
-			<LanguageProvider>
-				<ThemeProvider>
-					<PageContent>
-						<Navbar />
-						<Form />
-					</PageContent>
-				</ThemeProvider>
-			</LanguageProvider>
-		);
-	}
+function App() {
+	return (
+		<LanguageProvider>
+			<ThemeProvider>
+				<PageContent>
+					<Navbar />
+					<Form />
+				</PageContent>
+			</ThemeProvider>
+		</LanguageProvider>
+	);
 }
 
 export default App;
